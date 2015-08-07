e894fed8547b297e6778f36c905b08d6f92cf4b0
amitgandhinz <amit.gandhi@rackspace.com>
2015-08-07 15:34:13 -0400

    Added Tipboard dashboard tiles

--- dashboard/data_collector.py
+++ dashboard/data_collector.py
@@ -1,15 +1,27 @@
 import json
 import time
+import datetime
+from collections import deque
 import requests
 
 
-def run():
-
+def push_tile(tile_type, tile_key, data):
     tipboard_api_key = "b721f623a67441d68237eb1724f8a3cb"
     tipboard_api_version = "v0.1"
     tipboard_host = "http://localhost:7272"
     tipboard_url = "{0}/api/{1}/{2}/push".format(
         tipboard_host, tipboard_api_version, tipboard_api_key)
+
+    data_to_push = {
+        'tile': tile_type,
+        'key': tile_key,
+        'data': json.dumps(data),
+    }
+
+    requests.post(tipboard_url, data=data_to_push)
+
+
+def run():
 
     canary_api_host = "http://localhost:8889"
     canary_jobs_path = "/taskflow/jobs/poppy_service_jobs"
@@ -18,12 +30,14 @@ def run():
 
     print("Starting Canary Dashboard Data Collector")
 
+    jobcount_series = deque()
+
     while True:
         # request data from the Canary API
-        unclaimed = "0"
-        claimed = "0"
-        completed = "0"
-        running = "0"
+        unclaimed = 0
+        claimed = 0
+        completed = 0
+        total_count = 0
 
         response = requests.get(canary_api_url)
 
@@ -39,10 +53,7 @@ def run():
                 x for x in jobs_list if x.get('status') == "CLAIMED"])
             completed = len([
                 x for x in jobs_list if x.get('status') == "COMPLETED"])
-            running = len([x for x in jobs_list])
-
-        print "Unclaimed ({0}), Claimed ({1}), Completed ({2}), Running ({3})".format(
-            unclaimed, claimed, completed, running)
+            total_count = jobs[0].get('job_count', '0')
 
         # parse the data for the various supported tiles
         unclaimed_jobs = {
@@ -70,39 +81,27 @@ def run():
                 "description": x.get("_uuid")
             }
             for x in jobs_list]
+
+        jobcount_series.append(
+            [datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+             total_count])
+
+        if len(jobcount_series) > 300:
+            jobcount_series.popleft()
+
+        job_series_data = {
+            "subtitle": "Jobs Over Time",
+            "description": "",
+            "series_list": [list(jobcount_series)]
+        }
+
         # post the data to the tipboard api
 
-        # UNCLAIMED JOBS
-        data_to_push = {
-            'tile': 'just_value',
-            'key': "unclaimed_jobs",
-            'data': json.dumps(unclaimed_jobs),
-        }
-        requests.post(tipboard_url, data=data_to_push)
-
-        # CLAIMED JOBS
-        data_to_push = {
-            'tile': 'just_value',
-            'key': "claimed_jobs",
-            'data': json.dumps(claimed_jobs),
-        }
-        requests.post(tipboard_url, data=data_to_push)
-
-        # COMPLETED JOBS
-        data_to_push = {
-            'tile': 'just_value',
-            'key': "completed_jobs",
-            'data': json.dumps(completed_jobs),
-        }
-        requests.post(tipboard_url, data=data_to_push)
-
-        # RUNNING JOBS
-        data_to_push = {
-            'tile': 'fancy_listing',
-            'key': "running_jobs",
-            'data': json.dumps(running_jobs),
-        }
-        requests.post(tipboard_url, data=data_to_push)
+        push_tile('just_value', "unclaimed_jobs", unclaimed_jobs)
+        push_tile('just_value', "claimed_jobs", claimed_jobs)
+        push_tile('just_value', "completed_jobs", completed_jobs)
+        push_tile('fancy_listing', "running_jobs", running_jobs)
+        push_tile('line_chart', "job_series", job_series_data)
 
         # sleep for interval
         print "Posted Data.  Now Sleep."
